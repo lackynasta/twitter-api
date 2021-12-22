d7a2b2c369ec3ca473ccadbb653cc031503f46a0
lackynasta <lackynasta@gmail.com>
2021-12-22 14:36:42 +0300

    Nouveau projet Flask

---
+++ app/apis/tweets.py
@@ -0,0 +1,12 @@
+# app/apis/tweets.py
+# pylint: disable=missing-docstring
+
+from flask_restx import Namespace, Resource
+
+api = Namespace("tweets")
+
+
+@api.route("/hello")
+class TweetResource(Resource):
+    def get(self):
+        return "Goodbye from the 'tweets' namespace!"
